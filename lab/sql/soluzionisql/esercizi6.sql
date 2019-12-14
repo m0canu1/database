5b328227b25160caee1772c8d0750be04e3ef9b3
alex <alexandrumocanu9@gmail.com>
2019-12-14 21:35:43 +0100

    fff

--- lab/sql/soluzionisql/esercizi6.sql
+++ lab/sql/soluzionisql/esercizi6.sql
@@ -25,19 +25,38 @@ where not exists(select *
 --Suggerimento
 select *
 from p
-where not exists(select *
-                 from sp
-                 where p.pnum = sp.pnum
-                   and sp.snum = 'S2');
+where pnum not in (select pnum
+                   from sp
+                   where sp.pnum = p.pnum
+                     and sp.snum = 'S2');
 
 --Finale
-
+select s.sname
+from s
+where not exists(select *
+                 from p
+                 where pnum not in (select pnum
+                                    from sp
+                                    where sp.pnum = p.pnum
+                                      and sp.snum = s.snum)
+    );
 
 --Elenca i nomi dei fornitori che forniscono il prodotto ‘P1’.
+select s.sname
+from s
+where exists(select *
+             from sp
+             where s.snum = sp.snum
+               and pnum = 'P1');
 
 
 --Elenca i nomi dei fornitori che non forniscono il prodotto ‘P4’.
-
+select s.sname
+from s
+where not exists(select *
+                 from sp
+                 where s.snum = sp.snum
+                   and pnum = 'P4');
 
 --Trovare i nomi dei fornitori che forniscono almeno tutti i prodotti forniti da S2 (senza utilizzare operatori aggregati)
 --(suggerimento: scrivere prima una query che trovi i prodotti forniti da S2 ma non da S3 e poi generalizzare su ogni fornitore).
@@ -45,9 +64,33 @@ where not exists(select *
 --Sugerrimento
 
 
+select sp.pnum
+from sp
+where snum = 'S2'
+    except
+select sp.pnum
+from sp
+where snum = 'S3';
+
 --FINALE
+select s.sname, s.snum
+from s
+where not exists(select sp.pnum
+                 from sp
+                 where snum = 'S2'
+                     except
+                 select sp.pnum
+                 from sp
+                 where sp.snum = s.snum);
 
-
-
+SELECT S.SName
+FROM S
+WHERE NOT EXISTS(
+        SELECT *
+        FROM SP
+        WHERE SP.SNum = 'S2'
+          AND SP.PNum NOT IN (SELECT SP1.PNum
+                              FROM SP SP1
+                              WHERE SP1.SNum = S.SNum));
 
 
